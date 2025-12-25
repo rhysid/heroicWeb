3ef879ba941259530f5de1b0afb32fd095ac1410
Rhys_ <76148201+rhysid@users.noreply.github.com>
2025-12-25 13:15:10 +0700

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const axios = require('axios');
-const app = express()
-
-app.get('/', async(req,res)=>{
-    res.send(`<title>Heroic Coming Soon</title><p>Heroic Coming Soon</p>`)
-})
-app.listen(80, console.log("Server on 80"))
